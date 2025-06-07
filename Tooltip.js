eb816efa8430493db62b562b06e905bde2c0335d
janderson-costa <jandersonfcosta@outlook.com>
2025-06-07 17:26:35 -0300

    ---

--- Tooltip.js
+++ Tooltip.js
@@ -28,19 +28,93 @@ export default function Tooltip(targetElement, options) {
 		targetElement.appendChild($tooltip);
 		targetElement.addEventListener('mouseover', show);
 		targetElement.addEventListener('mouseout', hide);
+
+		$tooltip.addEventListener('mouseout', event => {
+			event.stopPropagation();
+
+			// Impede que seja fechado ao passar o mouse sobre outros elementos dentro do tooltip
+			if (event.relatedTarget && event.relatedTarget.closest('.tooltip')) return;
+
+			hide();
+		});
+		$tooltip.addEventListener('click', event => {
+			event.stopPropagation();
+			event.preventDefault();
+		});
 	}
 
 	function show() {
 		$content.innerHTML = options.text;
-		$tooltip.style.fontSize = options.fontSize;
 		$tooltip.style.padding = typeof options.offset == 'number' ? options.offset + 'px' : 'initial';
-		$content.style.maxWidth = typeof options.maxWidth == 'number' ? options.maxWidth + 'px' : 'auto';
+
+		if (options.style) {
+			style($content, options.style);
+		}
+
 		$tooltip.classList.add('tooltip-visible');
 	}
 
 	function hide() {
 		$tooltip.classList.remove('tooltip-visible');
 		$content.innerHTML = '';
-		$content.style.maxWidth = 'auto';
+		$content.style = null;
 	}
+}
+
+function style(element, style = {}) {
+	const pxProps = new Set([
+		'borderBottomLeftRadius',
+		'borderBottomRightRadius',
+		'borderBottomWidth',
+		'borderLeftWidth',
+		'borderRadius',
+		'borderRightWidth',
+		'borderTopLeftRadius',
+		'borderTopRightRadius',
+		'borderTopWidth',
+		'borderWidth',
+		'bottom',
+		'columnGap',
+		'fontSize',
+		'gap',
+		'height',
+		'left',
+		'letterSpacing',
+		'lineHeight',
+		'margin',
+		'marginBottom',
+		'marginLeft',
+		'marginRight',
+		'marginTop',
+		'maxHeight',
+		'maxWidth',
+		'minHeight',
+		'minWidth',
+		'outlineWidth',
+		'padding',
+		'paddingBottom',
+		'paddingLeft',
+		'paddingRight',
+		'paddingTop',
+		'right',
+		'rowGap',
+		'top',
+		'translateX',
+		'translateY',
+		'translateZ',
+		'width',
+	]);
+
+	const processedStyle = {};
+
+	for (const [prop, value] of Object.entries(style)) {
+		// Se o valor for um número, adiciona 'px' no final
+		if (pxProps.has(prop) && typeof value == 'number') {
+			processedStyle[prop] = `${value}px`;
+		} else {
+			processedStyle[prop] = value;
+		}
+	}
+
+	Object.assign(element.style, processedStyle);
 }
